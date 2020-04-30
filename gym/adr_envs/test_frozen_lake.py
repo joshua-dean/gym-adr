b2920a8fed83f7abc28ab928164c7cb0665e2915
Joshua Dean <deanx252@umn.edu>
2020-04-30 11:47:26 -0500

    Tested memoization optimization on FrozenLake

--- gym/adr_envs/test_frozen_lake.py
+++ gym/adr_envs/test_frozen_lake.py
@@ -2,6 +2,8 @@ import torch
 from gym.envs.toy_text.frozen_lake import FrozenLakeEnv
 from frozen_lake_adr import Direction, FrozenLakeADREnv
 from utils import memoized
+import random 
+import time 
 
 def test_original_env():
     env = FrozenLakeEnv(is_slippery=True)
@@ -11,18 +13,45 @@ def test_original_env():
         env.render()
         obs, rew, done, info = env.step(act)
 
-def test_obs_speed():
-    env = FrozenLakeADREnv(None)
-    for _ in range(10):
+# This is a questionable and non-comprehensive way to measure performance
+# Notes: For many steps or environment reuse, memoizing sees anwhere from 1-5% performance increase
+# For low step counts and/or single use environments this ends up being slower
+# Conclusion: Memoize only if an environment instance will persist for a large amount of time (>10 runs / >1000 timesteps)
+# This may be unlikely in the ADR case
+# Only tested for window size of 2 (3x3)
+def test_obs_transform_mem_speed():
+    """
+    Transition function uses ints but our observation needs a window view of the map based on position
+    This tests effectiveness of memoizing the function that produces that observation.
+    """
+    n_runs = 100
+    n_steps = 1000
+    actions = [random.choice(list(Direction)) for _ in range(n_steps)]
+
+    env = FrozenLakeADREnv(None, map_name="4x4")
+    normal_start = time.time()
+    for _ in range(n_runs):
         obs = env.reset()
-        act = Direction.RIGHT 
-        for _ in range(100):
-            env.render()
-            print(obs)
+        for idx in range(n_steps):
+            act = actions[idx]
             obs, rew, done, info = env.step(act)
+    normal_end = time.time()
+
+    env.expand_obs = memoized(env.expand_obs)
+    mem_start = time.time()
+    for _ in range(n_runs):
+        obs = env.reset()
+        for idx in range(n_steps):
+            act = actions[idx]
+            obs, rew, done, info = env.step(act)
+    mem_end = time.time()
+
+    print("Vanilla : {}s".format(normal_end - normal_start))
+    print("Memoized : {}s".format(mem_end - mem_start))
+
 
 
 if __name__ == "__main__":
-    test_obs_speed()
+    test_obs_transform_mem_speed()
         
     
--- gym/adr_envs/utils.py
+++ gym/adr_envs/utils.py
@@ -10,20 +10,20 @@ class memoized(object):
     def __init__(self, func):
             self.func = func
             self.cache = {}
-        def __call__(self, *args):
-            if not isinstance(args, collections.Hashable):
+    def __call__(self, *args):
+        if not isinstance(args, collections.Hashable):
             # uncacheable. a list, for instance.
             # better to not cache than blow up.
             return self.func(*args)
-            if args in self.cache:
+        if args in self.cache:
             return self.cache[args]
-            else:
+        else:
             value = self.func(*args)
             self.cache[args] = value
             return value
-        def __repr__(self):
-            '''Return the function's docstring.'''
-            return self.func.__doc__
-        def __get__(self, obj, objtype):
-            '''Support instance methods.'''
-            return functools.partial(self.__call__, obj)
+    def __repr__(self):
+        '''Return the function's docstring.'''
+        return self.func.__doc__
+    def __get__(self, obj, objtype):
+        '''Support instance methods.'''
+        return functools.partial(self.__call__, obj)
